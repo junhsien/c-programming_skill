6e3deb6d7b7b1f58857b6dfe47d384afc77a30d0
junhsien <william201129@gmail.com>
2022-04-28 11:46:06 +0800

    Update c_bitwise_integer_absolute_value.cpp

--- bitwise/c_bitwise_integer_absolute_value.cpp
+++ bitwise/c_bitwise_integer_absolute_value.cpp
@@ -8,10 +8,15 @@ using namespace std;
 int main()
 {
   int a = -4286;
+  
+  // method 1
   // mask  = a >> 31 -> (mask ^ a) - mask
   int abs = (a >> 31 ^ a - (a >> 31);
   
-  printf("a=%d, abs=%d\n", a, abs);
+  // method 2 (use std library)
+  int n = abs(a);
+             
+  printf("a=%d, abs=%d, n\n", a, abs, n);
 
 
   return 0;
